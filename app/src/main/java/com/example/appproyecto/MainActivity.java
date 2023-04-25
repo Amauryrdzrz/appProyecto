6c3e54ea681b679dbd48887984de2fa23d05622c
JaredRiesco25 <jaredriesco25@outlook.com>
2023-04-24 22:59:59 -0600

    Scan
    Se subió la implementación para lector QR

--- app/src/main/java/com/example/appproyecto/MainActivity.java
+++ app/src/main/java/com/example/appproyecto/MainActivity.java
@@ -5,8 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +25,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
+        btnScan = findViewById(R.id.btnScan);
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+
+                IntentIntegrator integrador = new IntentIntegrator(MainActivity.this);
+                integrador.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrador.setPrompt("Lector");
+                integrador.setCameraId(0);
+                integrador.setBeepEnabled(true);
+                integrador.setBarcodeImageEnabled(true);
+                integrador.initiateScan();
+
             }
-        },3000);
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null){
+            if(result.getContents() == null){
+                Toast.makeText(this, "Lectura Cancelada", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, result.getContents() , Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
